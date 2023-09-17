95dd0a4fddc5ab6c4a6949e19a9235f66092c9ad
Avikmahato <128340287+Avikmahato@users.noreply.github.com>
2023-09-18 04:52:53 +0530

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,66 @@
+from turtle import *
+from time import *
+from os import *
+t=Turtle()
+digi=Turtle()
+name=Turtle()
+t.speed(.01)
+s=getscreen()
+digi.ht()
+ht()
+name.ht()
+t.penup()
+t.pensize(2)
+t.goto(-200,0)
+t.pendown()
+t.pencolor('black')
+t.fillcolor('greenyellow')
+t.begin_fill()
+# Time Variable
+hou=int(input("Enter Hours    :"))
+minu=int(input("Enter Minutes  :"))
+sec=int(input("Enter Second   :"))
+for i in range(2):
+    t.fd(300)
+    t.lt(90)
+    t.fd(150)
+    t.lt(90)
+t.end_fill()
+t.penup()
+t.goto(-180,20)
+t.pendown()
+t.pencolor('black')
+t.fillcolor('yellow')
+t.begin_fill()
+for i in range(2):
+    t.fd(260)
+    t.lt(90)
+    t.fd(110)
+    t.lt(90)
+t.end_fill()
+name.penup()
+name.goto(-310,160)
+name.pendown()
+name.pencolor('aquamarine')
+name.write('CREATED BY AVIK MAHATO',font=('verdana',25,'bold'))
+t.penup()
+t.goto(-107,.5)
+t.pencolor('Darkblue')
+t.write('STOPWATCH',font=('arial',12,'bold'))
+digi.ht()
+t.ht()
+digi.penup()
+digi.goto(-156,50)
+digi.pendown()
+while hou>0 or minu>0 or sec>-1:
+    if sec<=0 and minu !=0:
+        minu-=1
+        sec=59
+    if minu<=0 and hou !=-1 and hou!=0:
+        hou-=1
+        minu=59
+        sec=59
+    digi.clear()
+    digi.write('%d : %d : %d '%(hou,minu,sec),font=('vedana',35,'bold'))
+    sec-=1
+    sleep(1)
